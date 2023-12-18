2d04140ac0f519c824ccd37eb72d5f68d43f8658
shahd <shahdhassan42@gmail.com>
2023-12-18 19:16:59 +0200

    initial

---
+++ get.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int sum(int, int);
+int subtract(int z, int f);
+int main() {
+	cout << " hello";
+	int sum(int x, float y) {
+		return x + y;
+	}
+	int subtract(int z, int f) {
+		z = f + 2;
+		f = 0 + 1;
+		cout << "hsahd";
+		int x = 0;
+		return z - f;
+	}
+	return 0;
+}
